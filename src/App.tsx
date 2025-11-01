089bd1b37facf9d986912e6dcec2af0449369c6f
Xzonn <Xzonn@outlook.com>
2025-11-01 11:17:27 +0800

    feat: 删除旧的目录组件

--- src/App.tsx
+++ src/App.tsx
@@ -33,7 +33,6 @@ const App: React.FC = () => (
           </div>
         </div>
       </main>
-      {/* <TableOfContents /> */}
     </div>
     <Footer />
   </Router>
--- src/components/site/index.ts
+++ src/components/site/index.ts
@@ -1,5 +1,4 @@
 export * from "./Header";
 export * from "./Notice";
 export * from "./Sidebar";
-export * from "./TableOfContents";
 export * from "./Footer";
